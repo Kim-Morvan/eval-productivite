2607d0f480df044bddf44dd82e75587b5b5aff84
Kim MORVAN <morvan.kim@gmail.com>
2022-01-29 13:17:21 +0100

    7 : Renforcer le code

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-import express from "express"
-import path from "path"
-import { fileURLToPath } from "url"
-import cors from "cors"
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import sanitizer from "sanitizer";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-const port = 8070
-const host = "127.0.0.1"
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const port = 8070;
+const host = "127.0.0.1";
 
-const app = express()
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(express.static(path.join(dirname, "public")))
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static(path.join(dirname, "public")));
 app.use(
   "/favicon.ico",
   express.static(path.join(dirname, "public", "images", "favicon.png"))
-)
+);
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(dirname) }, (err) => {
-    if (err) throw new Error(err)
-  })
-})
+  res.status(200).sendFile("index.html", { root: path.join(dirname) }, (err) => {
+    if (err) throw new Error(err);
+  });
+});
 
 app.post("/comment", (req, res) => {
-  const comment = req.body.message
-  res.send(comment)
-})
+  const comment = sanitizer.sanitize(req.body.message);
+  comment ? res.status(200).send(comment) : res.status(401).send("<h1>Script non autorisé</>");
+
+  // if (comment) {
+  //   return res.status(200).send(comment);
+  // }
+  // res.status(401).send("<h1>Script non autorisé</>");
+});
 
 app.listen(port, host, () => {
-  console.info(`Server started @ http://${host}:${port}.`)
-})
+  console.info(`Server started @ http://${host}:${port}.`);
+});
